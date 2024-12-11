575471a522e835e33756540fd92b921b936a64bd
Windows <Windows@LAPTOP-IQ8TJ0SI>
2024-12-11 11:56:23 +0700

    Add project files.

---
+++ WindowsFormsString/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtFullname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSplitName = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(249, 219);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(319, 22);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(249, 263);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(319, 22);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(249, 180);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(319, 22);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // txtFullname
+            // 
+            this.txtFullname.Location = new System.Drawing.Point(249, 86);
+            this.txtFullname.Name = "txtFullname";
+            this.txtFullname.Size = new System.Drawing.Size(319, 22);
+            this.txtFullname.TabIndex = 3;
+            this.txtFullname.Text = "นายหนวด สามหมอ";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(122, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ชื่อ - สกุล";
+            // 
+            // btnSplitName
+            // 
+            this.btnSplitName.Location = new System.Drawing.Point(625, 86);
+            this.btnSplitName.Name = "btnSplitName";
+            this.btnSplitName.Size = new System.Drawing.Size(81, 199);
+            this.btnSplitName.TabIndex = 5;
+            this.btnSplitName.Text = "แยกชื่อสกุล";
+            this.btnSplitName.UseVisualStyleBackColor = true;
+            this.btnSplitName.Click += new System.EventHandler(this.btnSplitName_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(122, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "คำนำหน้า";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(122, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ชื่อ ";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(122, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "สกุล";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSplitName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFullname);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtFullname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSplitName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsString/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSplitName_Click(object sender, EventArgs e)
+        {
+            string fullname="" , firstname="" , lastname="" , title="";
+            int x = 0;
+            fullname = txtFullname.Text.Trim();
+
+            if (fullname.StartsWith("นางสาว"))
+            {
+                x = 6;
+                title = fullname.Substring(0, x);
+               //fullname = fullname.Substring(6).Trim();
+            }
+            else if (fullname.StartsWith("นาง") || fullname.StartsWith("นาย"))
+            {
+                x = 3;
+                title=fullname.Substring(0, x);
+              //  fullname = fullname.Substring(3).Trim();
+            }
+            fullname = fullname.Substring(x).Trim();
+            //MessageBox.Show(title + "\n" + fullname);
+
+            x= fullname.IndexOf(" ");
+
+            firstname = fullname.Substring(0,x).Trim();
+
+            lastname = fullname.Substring(x).Trim();
+
+            txtTitle.Text = title;
+            txtFirstName.Text = firstname;
+            txtLastName.Text = lastname;
+        }
+
+    }
+}
